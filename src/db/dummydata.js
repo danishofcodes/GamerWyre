774b4282fd0a78710105c64df47171770ed61207
danish <danishofcodes@gmail.com>
2024-06-09 15:01:43 +0530

    Dummy Data to Populate the Product Stock in ecom shop

---
+++ src/db/dummydata.js
@@ -0,0 +1,112 @@
+
+export const productsdata = [
+        {
+          img: "image_link",
+          title: "PlayStation 5 Controller",
+          rating: 4.8,
+          price: 6699.99,
+          company: "Sony",
+          model: "DualSense",
+          category: "Gaming Accessories"
+        },
+        {
+          img: "image_link",
+          title: "Nintendo Switch",
+          rating: 4.7,
+          price: 12099.99,
+          company: "Nintendo",
+          model: "Switch",
+          category: "Gaming Console"
+        },
+        {
+          img: "image_link",
+          title: "Logitech G Pro X Gaming Headset",
+          rating: 4.9,
+          price: 12099.99,
+          company: "Logitech",
+          model: "G Pro X",
+          category: "Gaming Accessories"
+        },
+        {
+          img: "image_link",
+          title: "Xbox Elite Wireless Controller Series 2",
+          rating: 4.6,
+          price: 17599.99,
+          company: "Microsoft",
+          model: "Elite Series 2",
+          category: "Gaming Accessories"
+        },
+        {
+          img: "image_link",
+          title: "Razer BlackWidow Elite Mechanical Gaming Keyboard",
+          rating: 4.5,
+          price: 1699.99,
+          company: "Razer",
+          model: "BlackWidow Elite",
+          category: "Gaming Accessories"
+        },
+        {
+          img: "image_link",
+          title: "FIFA 22",
+          rating: 4.3,
+          price: 5099.99,
+          company: "EA Sports",
+          model: "Standard Edition",
+          category: "Video Game"
+        },
+        {
+          img: "image_link",
+          title: "Corsair Void RGB Elite Wireless Gaming Headset",
+          rating: 4.7,
+          price: 1990.99,
+          company: "Corsair",
+          model: "Void RGB Elite",
+          category: "Gaming Accessories"
+        },
+        {
+          img: "image_link",
+          title: "SteelSeries QcK Gaming Surface - Medium",
+          rating: 4.8,
+          price: 1400.99,
+          company: "SteelSeries",
+          model: "QcK",
+          category: "Gaming Accessories"
+        },
+        {
+          img: "image_link",
+          title: "Assassin's Creed Valhalla",
+          rating: 4.6,
+          price: 5099.99,
+          company: "Ubisoft",
+          model: "Standard Edition",
+          category: "Video Game"
+        },
+        {
+          img: "image_link",
+          title: "HyperX Cloud II Gaming Headset",
+          rating: 4.7,
+          price: 999.99,
+          company: "HyperX",
+          model: "Cloud II",
+          category: "Gaming Accessories"
+        },
+        {
+          img: "image_link",
+          title: "Samsung Odyssey G7 Gaming Monitor",
+          rating: 4.9,
+          price: 6699.99,
+          company: "Samsung",
+          model: "Odyssey G7",
+          category: "Gaming Accessories"
+        },
+        {
+          img: "image_link",
+          title: "Cyberpunk 2077",
+          rating: 4.1,
+          price: 5099.99,
+          company: "CD Projekt",
+          model: "Standard Edition",
+          category: "Video Game"
+        }
+      ];
+
