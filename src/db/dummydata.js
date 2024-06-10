fb5b992e779e901a7c5b352d0c77776012e65600
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-06-10 18:45:26 +0530

    added id's and Image Links

--- src/db/dummydata.js
+++ src/db/dummydata.js
@@ -1,112 +1,178 @@
+import image1 from '../assets/Images/assasinsCreed-V.png'
+import image2 from '../assets/Images/Cyberpunk.png'
+import image3 from '../assets/Images/NintendoSwitch.png'
+import image4 from '../assets/Images/LogitechGProX.png'
+import image5 from '../assets/Images/XboxEliteWireless.png'
+import image6 from '../assets/Images/CorsairVoidRGB.png'
+import image7 from '../assets/Images/razerblackwidow.png'
+import image8 from '../assets/Images/fifa22.png'
+import image9 from '../assets/Images/SteelSeriesQcK.png'
+import image10 from '../assets/Images/HyperXCloudII.png'
+import image11 from '../assets/Images/Odessey.png'
+import image12 from '../assets/Images/ps5.png'
+import image13 from '../assets/Images/controllerPS.png'
+
+
+
+
+
+
+
+
 
 export const productsdata = [
         {
-          img: "image_link",
+          id:1,
+          img: image13,
           title: "PlayStation 5 Controller",
           rating: 4.8,
           price: 6699.99,
           company: "Sony",
           model: "DualSense",
-          category: "Gaming Accessories"
+          category: "Gaming Accessories",
+          picked : false
         },
         {
-          img: "image_link",
+          id:2,
+          img: image3,
           title: "Nintendo Switch",
           rating: 4.7,
           price: 12099.99,
           company: "Nintendo",
           model: "Switch",
-          category: "Gaming Console"
+          category: "Gaming Console",
+          picked : false
         },
         {
-          img: "image_link",
+          id:3,
+          img: image4,
           title: "Logitech G Pro X Gaming Headset",
           rating: 4.9,
           price: 12099.99,
           company: "Logitech",
           model: "G Pro X",
-          category: "Gaming Accessories"
+          category: "Gaming Accessories",
+          picked : false
+
         },
+  
         {
-          img: "image_link",
-          title: "Xbox Elite Wireless Controller Series 2",
-          rating: 4.6,
-          price: 17599.99,
-          company: "Microsoft",
-          model: "Elite Series 2",
-          category: "Gaming Accessories"
-        },
-        {
-          img: "image_link",
+          id:4,
+          img: image7,
           title: "Razer BlackWidow Elite Mechanical Gaming Keyboard",
           rating: 4.5,
           price: 1699.99,
           company: "Razer",
           model: "BlackWidow Elite",
-          category: "Gaming Accessories"
+          category: "Gaming Accessories",
+          picked : false
+
         },
         {
-          img: "image_link",
+          id:5,
+          img: image8,
           title: "FIFA 22",
           rating: 4.3,
           price: 5099.99,
           company: "EA Sports",
           model: "Standard Edition",
-          category: "Video Game"
+          category: "Video Game",
+          picked : false
+
         },
         {
-          img: "image_link",
+          id:6,
+          img: image6,
           title: "Corsair Void RGB Elite Wireless Gaming Headset",
           rating: 4.7,
           price: 1990.99,
           company: "Corsair",
           model: "Void RGB Elite",
-          category: "Gaming Accessories"
+          category: "Gaming Accessories",
+          picked : false
+
         },
         {
-          img: "image_link",
+          id:7,
+          img: image9,
           title: "SteelSeries QcK Gaming Surface - Medium",
           rating: 4.8,
           price: 1400.99,
           company: "SteelSeries",
           model: "QcK",
-          category: "Gaming Accessories"
+          category: "Gaming Accessories",
+          picked : false
+
         },
         {
-          img: "image_link",
+          id:8,
+          img: image1,
           title: "Assassin's Creed Valhalla",
           rating: 4.6,
           price: 5099.99,
           company: "Ubisoft",
           model: "Standard Edition",
-          category: "Video Game"
+          category: "Video Game",
+          picked : false
+
         },
         {
-          img: "image_link",
+          id:9,
+          img:image10,
           title: "HyperX Cloud II Gaming Headset",
           rating: 4.7,
           price: 999.99,
           company: "HyperX",
           model: "Cloud II",
-          category: "Gaming Accessories"
+          category: "Gaming Accessories",
+          picked : false
+
         },
         {
-          img: "image_link",
+          id:10,
+          img: image11,
           title: "Samsung Odyssey G7 Gaming Monitor",
           rating: 4.9,
           price: 6699.99,
           company: "Samsung",
           model: "Odyssey G7",
-          category: "Gaming Accessories"
+          category: "Gaming Accessories",
+          picked : false
+
         },
         {
-          img: "image_link",
+          id:11,
+          img: image2,
           title: "Cyberpunk 2077",
           rating: 4.1,
           price: 5099.99,
           company: "CD Projekt",
           model: "Standard Edition",
-          category: "Video Game"
-        }
+          category: "Video Game",
+          picked : false
+
+        },
+        { id:12,
+          img: image12,
+          title: "PlayStation 5",
+          rating: 4.7,
+          price: 34099.99,
+          company: "Sony",
+          model: "Disc",
+          category: "Gaming Console",
+          picked : false
+        },
+        {
+          id:13,
+          img: image5,
+          title: "Xbox Elite Wireless Controller Series 2",
+          rating: 4.6,
+          price: 17599.99,
+          company: "Microsoft",
+          model: "Elite Series 2",
+          category: "Gaming Accessories",
+          picked : false
+
+        },
       ];
 
